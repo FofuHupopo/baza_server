f42e6cbfcf447a758408d49132d7709b901bf006
FofuHupopo <fofuhupopo@gmail.com>
2024-03-17 14:16:56 +0500

    synchronizer fix

--- synchronizer/models.py
+++ synchronizer/models.py
@@ -87,6 +87,7 @@ class ProductModificationModel(Base):
     slug = Column(String(255), nullable=True)
     color_id = Column(Integer, ForeignKey('product__product_color.id'), nullable=True)
     size_id = Column(Integer, ForeignKey('product__product_size.id'), nullable=True)
+    booked = Column(Integer, default=0)
     quantity = Column(Integer, default=0)
     weight = Column(Integer, default=0)
     visible = Column(Boolean, default=False)
