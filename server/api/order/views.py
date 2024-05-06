fc60ad25ec47577851072a9b5f8382e10eb34368
FofuHupopo <fofuhupopo@gmail.com>
2024-05-07 02:48:26 +0500

    UPD:

--- server/api/order/views.py
+++ server/api/order/views.py
@@ -439,6 +439,33 @@ class PaymentResponseSuccessView(APIView):
             order_model_id=order.pk
         )
         
+        print({
+            "order_id": str(order.id),
+            "receiving": order.receiving,
+            "address": order.address,
+            "code": order.code,
+            "is_express": order.is_express,
+            "floor_number": order.floor_number,
+            "apartment_number": order.apartment_number,
+            "intercom": order.intercom,
+            "user_id": order.user.pk,
+            "user_name": order.user.name,
+            "user_surname": order.user.surname,
+            "user_phone": order.user.phone,
+            "user_email": order.user.email,
+            "products": [
+                {
+                    "name": product.product_modification_model.product.name,
+                    "color": product.product_modification_model.color.name,
+                    "size": product.product_modification_model.size.name,
+                    "quantity": product.quantity,
+                    "code": product.product_modification_model.product.code,
+                    "url": f"https://thebaza.ru/products/{product.product_modification_model.slug[:-1]}"
+                }
+                for product in products
+            ]
+        })
+        
         BotServer.new_order({
             "order_id": str(order.id),
             "receiving": order.receiving,
